775bbf605d8f0dce25ea3894e70efdc78b9ce902
ObaidaSaleh <46760451+ObaidaSaleh@users.noreply.github.com>
2021-03-22 21:36:30 -0400

    fixed route for deployment

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const app = express();
 
 app.use(express.static(__dirname + "/pub"));
 
+app.get("/examples.html", (req, res) => {
+  res.send("<h1>This should be the root route!</h1>");
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   log(`Listening on port ${port}...`);
